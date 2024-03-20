2d8036bd65bebd2621cabcb592b5663d25af074d
Adrian <adrian.wojcik02@gmail.com>
2024-03-20 16:20:38 +0100

    Revert "aaaa"
    This reverts commit d109469144c92fc80e0e025d70caa83d4c6153ad.

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0cf3093e3758570223eb4eb8d593a2bd3bdd895")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de9be71de390847df64c9dfc1ba422b451dda428")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
