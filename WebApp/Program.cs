78dbf7c4f41dfbed726b380d950dadfd90602fa0
Adrian <adrian.wojcik02@gmail.com>
2024-03-21 10:32:07 +0100

    ConnectionToDataBase

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaulConnection")));
+
 
 var app = builder.Build();
 
