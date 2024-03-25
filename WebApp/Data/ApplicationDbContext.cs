bd14ab1e80f5a2f75f10966ce5254aa1f2e51215
Adrian <adrian.wojcik02@gmail.com>
2024-03-25 16:59:17 +0100

    Bootstra+newpage
    Dodanie nowego stylu,
    Utworzenie kategorii i wyświetlanie,
    Dodanie rekordów do bazy danych

--- WebApp/Data/ApplicationDbContext.cs
+++ WebApp/Data/ApplicationDbContext.cs
@@ -11,5 +11,14 @@ namespace WebApp.Data
         }
 
         public DbSet<Kategorie> Kategorie { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Kategorie>().HasData(
+                new Kategorie { KategorieId = 1, Name = "Akcja", DisplayOrder = 1 },
+                new Kategorie { KategorieId = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Kategorie { KategorieId = 3, Name = "Historia", DisplayOrder = 3 }
+                );
+        }
     }
 }
